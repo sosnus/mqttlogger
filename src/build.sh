8f47c1f2fee882dc91f943e86c1d16dec3b5cb92
Sosnus <sosnus15@gmail.com>
2024-06-26 07:53:55 +0200

    working with parameters

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-docker build --tag sosnus15/mqttlogger .
-# docker build --platform arm64 --tag sosnus15/mqttlogger .
+# docker build --tag sosnus15/mqttlogger .
+docker build --platform arm64 --tag sosnus15/mqttlogger .
 docker push sosnus15/mqttlogger
+docker pull sosnus15/mqttlogger
 # sudo docker run --rm --name mqttlogger_container sosnus15/mqttlogger
-sudo docker run --rm -e V_BROKER=10.10.10.210 -e VURL=google.com -e V_PORT=1883 -e V_DB_PATH=/tmp/testdir/mqtt-logs/ --name mqttlogger_container sosnus15/mqttlogger
+sudo docker run --rm -e V_BROKER=127.0.0.1 -e V_PORT=1883 -e V_DB_PATH=/tmp/testdir/mqtt-logs/ --name mqttlogger_container sosnus15/mqttlogger
