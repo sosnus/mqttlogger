22f64dd5d800e294876f27ae52326976f98f3f4a
Sosnus <sosnus15@gmail.com>
2024-06-29 20:36:40 +0200

    improve tags

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-tag="v15"
+tag="v17"
 echo $tag
 # sudo docker build --tag sosnus15/mqttlogger:v04 .
+# sudo docker push sosnus15/mqttlogger
 docker build --platform arm64 --tag sosnus15/mqttlogger:$tag .
 docker build --platform arm64 --tag sosnus15/mqttlogger .
-sudo docker push sosnus15/mqttlogger:$tag
-sudo docker push sosnus15/mqttlogger
 sudo docker push sosnus15/mqttlogger:latest
+sudo docker push sosnus15/mqttlogger:$tag
 sudo docker pull sosnus15/mqttlogger
