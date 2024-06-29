9f297c4421593f1e9ce667360686ee1370b449f6
Sosnus <sosnus15@gmail.com>
2024-06-29 12:57:22 +0200

    add new tag

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# docker build --tag sosnus15/mqttlogger .
-docker build --platform arm64 --tag sosnus15/mqttlogger .
-docker push sosnus15/mqttlogger
-docker pull sosnus15/mqttlogger
+# sudo docker build --tag sosnus15/mqttlogger:v04 .
+docker build --platform arm64 --tag sosnus15/mqttlogger:v07 .
+sudo docker push sosnus15/mqttlogger:v07
+sudo docker pull sosnus15/mqttlogger
 # sudo docker run --rm --name mqttlogger_container sosnus15/mqttlogger
-sudo docker run --rm -e V_BROKER=127.0.0.1 -e V_PORT=1883 -e V_DB_PATH=/tmp/testdir/mqtt-logs/ --name mqttlogger_container sosnus15/mqttlogger
+# sudo docker run --rm -e V_BROKER=127.0.0.1 -e V_PORT=1883 -e V_DB_PATH=/tmp/testdir/mqtt-logs/ --name mqttlogger_container sosnus15/mqttlogger
