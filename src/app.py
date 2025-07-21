6f16e878a6ffb0dd6a8f5e1107840d09458f7f8d
Sosnus <sosnus15@gmail.com>
2025-07-21 08:02:36 +0200

    fix datetime formatting

--- src/app.py
+++ src/app.py
@@ -44,7 +44,8 @@ def on_message(client, userdata, message):
     message_payload = message.payload.decode('utf-8',errors='replace')
     datawriter.insert_message(message_payload, message.topic)
     client.publish("mqttlogger/mqttlogger", f"msg from {message.topic} len={len(message_payload)} logged")
-    print(f">>> msg from {message.topic} len={len(message_payload)} logged {datetime.now()}")
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
+    print(f">>> msg from {message.topic} len={len(message_payload)} logged {timestamp}")
 # Define the callback function for when the client connects to the broker
 def on_connect(client, userdata, flags, rc, properties=None):
     if rc == 0:
